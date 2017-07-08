a48786edff2a9637306127654d86d0edeef7aba8
xiaoshenge <xiaoshengeer@gmail.com>
2017-07-08 15:58:48 +0000

    project use phpunit

---
+++ phpunit/bootstrap/autoload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoshenge
+ * Date: 2017/7/8
+ * Time: 下午10:45
+ */
+
+include  __DIR__ ."/../vendor/autoload.php";
---
+++ phpunit/tests/EmailTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\InvalidArgumentException;
+use src\Email;
+
+/**
+ * @covers Email
+ */
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
+
